9d51c5ff6a6305ecdb388ab28a540181aabd9d80
Awesome-Linter Automation <noreply@github.com>
2023-07-23 05:21:13 +0600

    up

---
+++ runner/banner.go
@@ -0,0 +1,33 @@
+package runner
+
+import (
+    "github.com/khulnasoft-labs/gologger"
+	updateutils "github.com/khulnasoft-labs/utils/update"
+)
+
+
+const banner = `
+    __    __  __       _  __
+   / /_  / /_/ /_____ | |/ /
+  / __ \/ __/ __/ __ \|   /
+ / / / / /_/ /_/ /_/ /   |
+/_/ /_/\__/\__/ .___/_/|_|
+             /_/
+`
+
+// Version is the current version of httpx
+const version = `v1.3.3`
+
+// showBanner is used to show the banner to the user
+func showBanner() {
+	gologger.Print().Msgf("%s\n", banner)
+	gologger.Print().Msgf("\t\tprojectdiscovery.io\n\n")
+}
+
+// GetUpdateCallback returns a callback function that updates httpx
+func GetUpdateCallback() func() {
+	return func() {
+		showBanner()
+		updateutils.GetUpdateToolCallback("httpx", version)()
+	}
+}
